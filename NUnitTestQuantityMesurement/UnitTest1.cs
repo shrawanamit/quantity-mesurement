b5f43fad2578052f171f54d554696c9de71492f2
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-20 01:01:05 +0530

    Added:TC 1.2 cheaing two inch varibal not eqal

--- NUnitTestQuantityMesurement/UnitTest1.cs
+++ NUnitTestQuantityMesurement/UnitTest1.cs
@@ -24,5 +24,20 @@ namespace NUnitTestQuantityMesurement
             Feet feet2 = new Feet(2.0);
             Assert.AreNotEqual(feet1, feet2);
         }
+        [Test]
+        public void Given0InchAnd0Inch_ShouldReturnEquals()
+        {
+            Inch inch1=new Inch(0.0);
+            Inch inch2 = new Inch(0.0);
+            Assert.AreEqual(inch1, inch2);
+        }
+        [Test]
+        public void Given0InchAnd0Inch_ShouldReturnNotEquals()
+        {
+            Inch inch1 = new Inch(0.0);
+            Inch inch2 = new Inch(1.0);
+            Assert.AreNotEqual(inch1, inch2);
+        }
+
     }
 }
---
+++ quantityMesurement/Inch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace quantityMesurement
+{
+    public class Inch
+    {
+        private readonly double value;
+
+        public Inch(double value)
+        {
+            this.value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Inch inch &&
+                   value == inch.value;
+        }
+    }
+}
