787b86fa1b1b82b018bd5b65badbd1829e8546a9
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-19 11:35:34 +0530

    welcome to quantity mesurement

---
+++ quantityMesurement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quantityMesurement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to quantity mesurement !!!");
+        }
+    }
+}
