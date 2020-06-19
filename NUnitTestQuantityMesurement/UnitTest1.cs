70d112fe062f7e7ac56cb82686e580b346fbba73
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-20 00:53:49 +0530

    Added:TC 1.2 cheaing two feet varibal not eqal

--- NUnitTestQuantityMesurement/UnitTest1.cs
+++ NUnitTestQuantityMesurement/UnitTest1.cs
@@ -14,8 +14,15 @@ namespace NUnitTestQuantityMesurement
         public void Given0FeetAnd0Feet_ShouldReturnEquals()
         {
             Feet feet1=new Feet(0.0);
-            Feet feet2= new Feet(0.0);
+            Feet feet2=new Feet(0.0);
             Assert.AreEqual(feet1, feet2);
+        }
+        [Test]
+        public void Given1FeetAnd2Feet_ShouldReturnNotEquals()
+        {
+            Feet feet1 = new Feet(1.0);
+            Feet feet2 = new Feet(2.0);
+            Assert.AreNotEqual(feet1, feet2);
         }
     }
 }
